16df4affca5bd71135de3c094d582a7cd24e5c1b
chollsak <koonnew8688.cookie@gmail.com>
2025-11-13 04:58:31 +0700

    no message

--- src/offense_detection/dto/update-offense_detection.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOffenseDetectionDto } from './create-offense_detection.dto';
-
-export class UpdateOffenseDetectionDto extends PartialType(CreateOffenseDetectionDto) {}
